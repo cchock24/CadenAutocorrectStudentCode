18ac3fffe361346db2dfbbf616937bf457a9a78d
Caden <caden.chock@menloschool.org>
2025-03-17 14:11:02 -0700

    hi

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,12 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |    Time     |                                                                                                                                                                                                                                   Update |
+|:--------|:-----------:|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5 |   9-10pm    |                                                                                                                                                                                               I tried to write abstract, efficient code. |
+| Mar 5   |  2:30-3:00  | Talked about it with Mr. Blick and Tyler. We talked about what if we made a substring of all possible outcomes if you were to substitute, add, or substract the words and then getting all real words from that long list of words yeah. |
+| Mar 7   | 12:35-1:45  |                                                                                                     Talked to Mr. Blick and Tyler explored the idea of using hash key/map. Taking pairs of letter hashing them adding word to that index |
+| Mar 12  | 12:25-12:55 |                                                                                                                                                                                          Started to work on the edit distance code stuff |
+| Mar 14  |  8:50-9:55  |                                                                                                                                                                                                                                          |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Autocorrect
@@ -8,18 +9,51 @@ import java.io.IOException;
  * A command-line tool to suggest similar words when given one not in the dictionary.
  * </p>
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Caden Chock
  */
 public class Autocorrect {
-
+    String[] dctionary;
+    int threshold;
+    String[] dictionary;
     /**
      * Constucts an instance of the Autocorrect class.
      * @param words The dictionary of acceptable words.
      * @param threshold The maximum number of edits a suggestion can have.
      */
     public Autocorrect(String[] words, int threshold) {
+        dictionary = words;
+       this.threshold = threshold;
+        // Make Trie of Dictionary
+        // Hash Key Pair
+
+        //Hash Table 28^2. 26 Letters + dash and apostrophe
+        ArrayList<Integer>[] table = new ArrayList[784];
 
     }
+
+    public int editDistance(String a, String b){
+        int[][] saved = new int[a.length()+1][b.length()+1];
+        //Base Case
+        for(int i = 0 ; i < a.length(); i++){
+            saved[0][i] = i;
+        }
+        for(int i = 0; i < b.length(); i++){
+            saved[i][0] = i;
+        }
+        for(int i = 1; i < a.length()+1; i++){
+            for(int j = 1; j < b.length()+1; j++){
+                if(a.charAt(i) == b.charAt(j)){
+                    saved[i][j] = saved[i-1][j-1];
+                }
+                else{
+                    int temp = Math.min(saved[i-1][j-1],saved[i-1][j]);
+                    saved[i][j] = Math.min(temp, saved[i][j-1]);
+                }
+            }
+        }
+        return 0;
+    }
+
 
     /**
      * Runs a test from the tester file, AutocorrectTester.
@@ -28,7 +62,14 @@ public class Autocorrect {
      * to threshold, sorted by edit distnace, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
-
+        // Make Array Arraylist to hold Possible Suggestions
+        ArrayList<String>[] words = new ArrayList[threshold];
+        for(int i = 0; i < dictionary.length; i++){
+            int edit = editDistance(dictionary[i], typed);
+            if(edit <= threshold){
+                words[edit].add(dictionary[i]);
+            }
+        }
         return new String[0];
     }
 
